9130882b56c07cf7177eeb88203f5e315bdeac8a
Ragelezz <raduneculaulostone@gmail.com>
2024-02-26 01:29:03 +0200

    das
    dasd

--- app.js
+++
@@ -1,5 +0,0 @@
-const showAnswer = (e) => {
-  // console.log(e);
-  const i = event.target.id.split("-").pop();
-  document.getElementsByClassName("faq-item")[i - 1].classList.toggle("active");
-};
